b1072378e29d5a896fded2ef003e68e0381bc7fe
Dariusz Jakubowski <celber@me.com>
2013-08-22 10:02:25 +0200

    Started rewriting to typescript, basefiles added

---
+++ simonts.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=simonts.js.map
---
+++ simonts.ts
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: celber
+ * Date: 22.08.2013
+ * Time: 10:00
+ * To change this template use File | Settings | File Templates.
+ */
